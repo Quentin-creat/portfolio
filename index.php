c51f15e7f347789a6cfb3dbe3e8d4cc321596779
Quentin Guillou <quentinguillou91@gmail.com>
2020-10-13 19:41:10 +0200

    add hom_left

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
-    include_once('header.html');
+include_once('header.html');
 ?>
 
-    <div id="home">
-        <div id="home_left">
+<div id="home">
+    <div id="home_left">
+        <div id="home_left_content">
+            <p id="intro" class="home_space">Hello, My Name Is</p>
+            <h1 id="name">QUENTIN</h1>
+            <h1 id="surname" class="home_space">GUILLOU</h1>
 
-            <p>Hello, My Name Is</p>
-            <h1>Quentin</h1>
-            <h1>Guillou</h1>
-
-            <div id="job">
+            <div id="job" class="home_space">
                 <div id="home_rect"></div>
                 <p>Junior full stack developer</p>
             </div>
-
-        </div>
-
-        <div id="home_right">
-
         </div>
     </div>
+
+    <div id="home_right">
+
+    </div>
+</div>
 
 
 
 <?php
-    include_once('footer.html');
+include_once('footer.html');
 ?>
