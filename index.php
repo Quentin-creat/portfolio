c0801491c0d886b21e0b9065cb72cf279a939ff6
Quentin Guillou <quentinguillou91@gmail.com>
2020-10-13 18:48:43 +0200

    init project +header

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    include_once('header.html');
+?>
+
+    <div id="home">
+        <div id="home_left">
+
+            <p>Hello, My Name Is</p>
+            <h1>Quentin</h1>
+            <h1>Guillou</h1>
+
+            <div id="job">
+                <div id="home_rect"></div>
+                <p>Junior full stack developer</p>
+            </div>
+
+        </div>
+
+        <div id="home_right">
+
+        </div>
+    </div>
+
+
+
+<?php
+    include_once('footer.html');
+?>
