bb1079bcf57422d75fe989d28d5f1603e6702395
Quentin Guillou <quentinguillou91@gmail.com>
2021-02-28 13:35:49 +0100

    Update projects styles

--- pages/accueil.php
+++ pages/accueil.php
@@ -198,7 +198,31 @@ include_once('includes/header.html');
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
-                <img src="./img/ProjectsPictures/unreal.png" class="card-img-top" alt="...">
+                <img src="./img/ProjectsPictures/unreal.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="../index.php?action=projet-unrealEngine" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./img/ProjectsPictures/nodejs.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="../index.php?action=projet-nodeJS" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./img/ProjectsPictures/wordpress.svg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="../index.php?action=projet-wordpress" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./img/ProjectsPictures/unreal.svg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
