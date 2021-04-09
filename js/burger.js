1ee24ee80d7e697c3de635f378a279b7779aaa48
Guillou Quentin <quentinguillou91@gmail.com>
2021-04-09 18:39:04 +0200

    rapide améliorations menu burger

--- js/burger.js
+++ js/burger.js
@@ -2,11 +2,13 @@
     $(".toOpen").on("click", function () {
       $(".hamburger-menu").toggleClass("menu");
       $(".menu-wrapper").toggleClass("toClose");
+      $(".menu-wrapper").text("✕");
       $(".menu-wrapper").removeClass("toOpen");
     });
     $(".toClose").on("click", function () {
         $("hamburger-menu").removeClass("menu");
         $(".menu-wrapper").removeClass("toClose");
+        $(".menu-wrapper").text("☰");
         $(".menu-wrapper").toggleClass("toOpen");
     })
   })();
